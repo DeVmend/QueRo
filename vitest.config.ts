1ad0c4397ef480c692bf30bf1acc7589100be058
FRIDAY-PP <friday-pp@users.noreply.github.com>
2026-02-17 07:33:37 +0000

    fix: Restore miniflare test environment

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     test: {
         globals: true,
+        environment: 'miniflare',
+        environmentOptions: {
+            modules: true,
+            compatibilityDate: '2024-01-01',
+        },
     },
 })
